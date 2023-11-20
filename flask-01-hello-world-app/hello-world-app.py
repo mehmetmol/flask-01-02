e7c0b437c63652383cf81d423ab97241ead77569
Mehmet Mol <mehmetmol42@gmail.com>
2023-11-20 23:15:29 +0300

    Second edition

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,7 +1,15 @@
 from flask import Flask 
 app = Flask(__name__)
 
-
+@app.route('/')
+def head():
+    return "Hello World!"
+@app.route('/second')
+def second():
+    return 'This is second page'
+@app.route('/third')
+def third():
+    return "this is third page"
 
 
 
--- flask-02-Jinja_Template/jinja.py
+++ flask-02-Jinja_Template/jinja.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return render_template("index.html", number1=1234, number2=7896)
 
-
-
-
-
+@app.route("/subpage")
+def number():
+    num1 = 12
+    num2 = 10
+    return render_template("body.html", value1=num1, value2=num2, sum=num1+num2)
 
 
 if __name__== "__main__":
